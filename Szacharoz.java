5afdf6caef981cf04c6a583fc0a8c6c626b664a1
csengekulker <k71608883429@ktch.hu>
2022-02-21 17:08:06 +0100

    data prints and input labels

---
+++ Szacharoz.java
@@ -0,0 +1,24 @@
+/*
+* File: Szacharoz.java
+* Author: Balogh Csenge
+* Copyright: 2022, Balogh Csenge
+* Group: Szoft_I_N
+* Date: 2022-02-21
+* Github: https://github.com/csengekulker/
+* Licenc: GNU GPL
+*/
+
+import java.util.Scanner;
+
+class Szacharoz {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Balogh Csenge\nSzoft_I_N\n2022.02.21");
+        System.out.println("Szacharóz");
+
+        System.out.print("Széntartalom > ");
+        System.out.print("Hidrogéntartalom > ");
+        System.out.print("Oxigéntartalom > ");
+    }
+} 
